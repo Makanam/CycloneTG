ad62c65d3266e27e449c92f099290e8217424d76
Makanam <makan.j7070@gmail.com>
2016-02-28 15:58:48 +0330

    Update version.lua

--- plugins/version.lua
+++ plugins/version.lua
@@ -1,27 +1,12 @@
---------------------------------------------------
---      ____  ____ _____                        --
---     |    \|  _ )_   _|___ ____   __  __      --
---     | |_  )  _ \ | |/ ·__|  _ \_|  \/  |     --
---     |____/|____/ |_|\____/\_____|_/\/\_|     --
---                                              --
---------------------------------------------------
---                                              --
---       Developers: @Josepdal & @MaSkAoS       --
---     Support: @Skneos,  @iicc1 & @serx666     --
---                                              --
---------------------------------------------------
-
 do
 
 function run(msg, matches)
-  return 'DBTeam Bot V1 Supergroups\nAn advanced Administration bot based on yagop/telegram-bot \n\nhttp://github.com/Josepdal/DBTeam\n\nDevelopers :\n@MaSkAoS\n@Josepdal\n\nSupport :\n@skneos\n@iicc1\n@Serx666' 
-  --[[ Checkout https://github.com/Josepdal/DBTeam
-  --GNU GPL v2 license.]]
+  return 'CycloneTG Bot V5\nAn advanced Administration bot based on yagop/telegram-bot \n\sudo:Makan\nnew version.]]
 end
 
 return {
   patterns = {
-    "^#version$"
+    "^version$"
   }, 
   run = run 
 }
